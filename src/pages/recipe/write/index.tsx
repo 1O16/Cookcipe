e1450a6f25dcf6278c5a87f851adc8d6886e803d
[1O16] <youiiyou1253@gmail.com>
2023-03-29 00:27:47 +0900

    feat: write recipe 페이지 ui

--- src/pages/recipe/write/index.tsx
+++ src/pages/recipe/write/index.tsx
@@ -9,7 +9,33 @@ const Write = () => {
     <>
       <Layout>
         <S.WriteHeader>레시피 작성</S.WriteHeader>
-        <S.WriteBody>.</S.WriteBody>
+        <S.WriteBody>
+          <S.ForImageContainer>
+            <S.RecipeFormContainer>
+              <S.PropsContainer>
+                <S.PropsTitle>레시피명</S.PropsTitle>
+                <S.WriteRecipeTitle placeholder="레시피명을 입력해주세요" />
+              </S.PropsContainer>
+              <S.PropsContainer>
+                <S.PropsTitle>레시피 소개</S.PropsTitle>
+                <S.WriteRecipeTitle placeholder="레시피 소개를 입력해주세요" />
+              </S.PropsContainer>
+              <S.PropsContainer>
+                <S.PropsTitle>카테고리</S.PropsTitle>
+                <S.CategorySelector>
+                  <option value="KoreanFood">한식</option>
+                </S.CategorySelector>
+                <S.PropsTitle style={{ marginLeft: "35px" }}>난이도</S.PropsTitle>
+                <S.CategorySelector>
+                  <option value="Hard">상</option>
+                  <option value="Normal">중</option>
+                  <option value="Easy">하</option>
+                </S.CategorySelector>
+              </S.PropsContainer>
+            </S.RecipeFormContainer>
+            <S.RecipeImage type="file" />
+          </S.ForImageContainer>
+        </S.WriteBody>
       </Layout>
     </>
   );
--- src/pages/recipe/write/styled.ts
+++ src/pages/recipe/write/styled.ts
@@ -4,13 +4,72 @@ export const WriteHeader = styled.div`
   width: 97%;
   height: 60px;
   display: flex;
-  align-items: center;
-  font-size: 22px;
+  font-size: 30px;
   font-weight: 600;
-  padding: 0 20px;
+  padding: 0 18.5px;
   border-bottom: 1px solid #848484;
 `;
 
 export const WriteBody = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   background-color: orange;
+  padding: 20px 0;
+  width: 100%;
+`;
+
+export const ForImageContainer = styled.div`
+  width: 80%;
+  display: flex;
+  justify-content: space-between;
+  background-color: gray;
+`;
+
+export const RecipeFormContainer = styled.div`
+  width: 75%;
+`;
+
+export const PropsContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+  background-color: #f5f5f5;
+  padding: 5px 0;
+`;
+
+export const PropsTitle = styled.p`
+  font-size: 20px;
+  width: 15%;
+`;
+
+export const WriteRecipeTitle = styled.input`
+  width: 450px;
+  height: 40px;
+  padding: 6px 10px 6px 15px;
+  font-size: 20px;
+  background-color: #e2e2e2;
+  border: 1px solid #848484;
+  border-radius: 5px;
+
+  &::placeholder {
+    font-size: 16px;
+    vertical-align: middle;
+  }
+`;
+
+export const RecipeImage = styled.input`
+  width: 200px;
+  height: 200px;
+  background-color: #f2f2f2;
+  border: 1px solid #e6e6e6;
+  border-radius: 5px;
+`;
+
+export const CategorySelector = styled.select`
+  width: 120px;
+  height: 40px;
+  border-radius: 5px;
+  font-size: 16px;
+  text-align: center;
 `;
