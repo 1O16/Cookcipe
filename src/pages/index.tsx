40778dc6046c6b1d7913f2d0f0a27f6ca0a9d99f
[1O16] <youiiyou1253@gmail.com>
2023-04-11 21:50:18 +0900

    remove: 사용하지 않는 파일 삭제

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -97,6 +97,11 @@ const HomePage: NextPage = () => {
         <S.PropsDesc>모든 레시피를 손쉽고 빠르게</S.PropsDesc>
         <S.CategoryButtonContainer>
           <CategoryButton filter={filterItems} filterStatus={filter} />
+          {filterdItems.map((filteredItem) => (
+            <h2 key={filteredItem.id}>
+              {filteredItem.title} : {filteredItem.category}
+            </h2>
+          ))}
         </S.CategoryButtonContainer>
       </S.CategoryContainer>
     </Layout>
@@ -104,71 +109,3 @@ const HomePage: NextPage = () => {
 };
 
 export default HomePage;
-
-{
-  /* <S.SlickContainer style={{ marginTop: "40px" }}>
-<Slick title="갓 작성된 따끈따끈한 레시피" titleInfo="가장 최근에 작성된 레시피들이에요">
-  <S.SlickItem>
-    <h3>{recipeTitle[0]} 11</h3>
-  </S.SlickItem>
-  <S.SlickItem>
-    <h3>{recipeTitle[1]}</h3>
-  </S.SlickItem>
-  <S.SlickItem>
-    <h3>{recipeTitle[2]}</h3>
-  </S.SlickItem>
-  <S.SlickItem>
-    <h3>{recipeTitle[3]}</h3>
-  </S.SlickItem>
-  <S.SlickItem>
-    <h3>{recipeTitle[4]}</h3>
-  </S.SlickItem>
-</Slick>
-</S.SlickContainer>
-<S.SlickContainer style={{ marginTop: "150px" }}>
-<Slick
-  title="가장 많은 분들이 본 레시피"
-  titleInfo="가장 조회수가 높은 레시피들이에요"
-  moreinfo="더 많은 레시피 보러가기"
->
-  <S.SlickItem>
-    <h3>1</h3>
-  </S.SlickItem>
-  <S.SlickItem>
-    <h3>2</h3>
-  </S.SlickItem>
-  <S.SlickItem>
-    <h3>3</h3>
-  </S.SlickItem>
-  <S.SlickItem>
-    <h3>4</h3>
-  </S.SlickItem>
-  <S.SlickItem>
-    <h3>5</h3>
-  </S.SlickItem>
-</Slick>
-</S.SlickContainer>
-<S.SlickContainer style={{ marginTop: "150px" }}>
-<Slick
-  title="가장 평가가 좋은 레시피"
-  titleInfo="가장 평점이 높은 레시피들이에요"
-  moreinfo="더 많은 레시피 보러가기"
->
-  <S.SlickItem>
-    <h3>1</h3>
-  </S.SlickItem>
-  <S.SlickItem>
-    <h3>2</h3>
-  </S.SlickItem>
-  <S.SlickItem>
-    <h3>3</h3>
-  </S.SlickItem>
-  <S.SlickItem>
-    <h3>4</h3>
-  </S.SlickItem>
-  <S.SlickItem>
-    <h3>5</h3>
-  </S.SlickItem>
-</Slick>
-</S.SlickContainer> */
-}
